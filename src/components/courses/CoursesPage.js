30a37cf0a9dcc3b095f99524d17e88e097ee9081
N.U <n.n.ushakova@gmail.com>
2020-11-16 23:46:20 -0500

    Load authors list

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/CourseActions';
+import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {  
     componentDidMount() {
-        this.props.actions.loadCourses().catch(error => {
-            alert("Loading courses failed"+ error);
-        });
+        if (this.props.courses.length === 0) {
+            this.props.actions.loadCourses().catch(error => {
+                alert("Loading courses failed"+ error);
+            });
+        }
+
+        if (this.props.authors.length === 0) {
+            this.props.actions.loadAuthors().catch(error => {
+                alert("Loading authors failed"+ error);
+            });
+        }
     }
     render() {
         return( 
@@ -22,20 +31,32 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
+    authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
     return {
-        courses: state.courses
+        courses: 
+        state.authors.length === 0 
+        ? [] 
+        : state.courses.map(course => {
+            return {
+                ...course,
+                authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+        }),
+        authors: state.authors
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(courseActions, dispatch)
-    };
+        actions: {
+            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import courses from "./courseReducer";//courseReduser function we named here as course
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-    courses: courses  
+    courses: courses,
+    authors: authors  
 });
 
 export default rootReducer;
