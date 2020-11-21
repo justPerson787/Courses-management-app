e6743c786cbed365f1ff0151c40d69c9c83488d3
N.U <n.n.ushakova@gmail.com>
2020-11-20 23:59:09 -0500

    Add api status actions

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import courses from "./courseReducer";//courseReduser function we named here as course
 import authors from './authorReducer';
+import apiCallsInProgress from './apiStatusReducer';
 
 const rootReducer = combineReducers({
     courses: courses,
-    authors: authors  
+    authors: authors,
+    apiCallsInProgress 
 });
 
 export default rootReducer;
