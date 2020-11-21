d05825ec7aa7510d483cdad567c2b4a8d85debf9
N.U <n.n.ushakova@gmail.com>
2020-11-21 00:12:42 -0500

    Add spinner to coursePage

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -30,17 +30,22 @@ class CoursesPage extends React.Component {
         return( 
         <>
             {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
-            <h2>Courses</h2>  
-            <Spinner />
-            <button 
-                style={{ marginBottom: 20}}
-                className="btn btn-primary add-course"
-                onClick={() => this.setState({ redirectToAddCoursePage: true })}
-            >
-                Add Course
-            </button>
+            <h2>Courses</h2> 
+            {this.props.loading ? (
+            <Spinner /> 
+            ) : (
+            <>
+                <button 
+                    style={{ marginBottom: 20}}
+                    className="btn btn-primary add-course"
+                    onClick={() => this.setState({ redirectToAddCoursePage: true })}
+                >
+                    Add Course
+                </button>
 
-            <CourseList courses={this.props.courses} />          
+                <CourseList courses={this.props.courses} />    
+            </>
+            )}    
         </>       
         );    
     }
@@ -49,7 +54,8 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
     authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+    actions: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
@@ -63,7 +69,8 @@ function mapStateToProps(state) {
                 authorName: state.authors.find(a => a.id === course.authorId).name
             };
         }),
-        authors: state.authors
+        authors: state.authors,
+        loading: state.apiCallsInProgress > 0
     };
 }
 
